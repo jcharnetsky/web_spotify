7f3d753af6508b00262628841ec194299cde8c44
Josh <jcharnetsky1@gmail.com>
2017-10-27 20:13:21 -0400

    add resizable sidebars

---
+++ app.js
@@ -0,0 +1,5 @@
+var app = angular.module('web_spotify', ['mc.resizer']);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.content = '';
+});
---
+++ resizer.js
@@ -0,0 +1,57 @@
+angular.module('mc.resizer', []).directive('resizer', function($document) {
+
+	return function($scope, $element, $attrs) {
+
+		$element.on('mousedown', function(event) {
+			event.preventDefault();
+
+			$document.on('mousemove', mousemove);
+			$document.on('mouseup', mouseup);
+		});
+
+		function mousemove(event) {
+
+			if ($attrs.resizer == 'vertical') {
+				// Handle vertical resizer
+				var x = event.pageX;
+				x = x * (100 / document.documentElement.clientWidth);
+
+				if ($attrs.resizerMax && x > $attrs.resizerMax) {
+					x = parseInt($attrs.resizerMax);
+				}
+
+				$element.css({
+					left: x + 'vw'
+				});
+
+				$($attrs.resizerLeft).css({
+					width: x + 'vw'
+				});
+				$($attrs.resizerRight).css({
+					left: (x + parseInt($attrs.resizerWidth)) + 'vw'
+				});
+
+			} else {
+				// Handle horizontal resizer
+				var y = window.innerHeight - event.pageY;
+				y = y * (100 / document.documentElement.clientHeight);
+
+				$element.css({
+					bottom: y + 'vh'
+				});
+
+				$($attrs.resizerTop).css({
+					bottom: (y + parseInt($attrs.resizerHeight)) + 'vh'
+				});
+				$($attrs.resizerBottom).css({
+					height: y + 'vh'
+				});
+			}
+		}
+
+		function mouseup() {
+			$document.unbind('mousemove', mousemove);
+			$document.unbind('mouseup', mouseup);
+		}
+	};
+});
