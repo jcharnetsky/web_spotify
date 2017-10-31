628b7eee59cf932392c007dd3f881e2e809d67b0
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-10-30 22:16:27 -0400

    Initial spring boot builg

---
+++ src/main/java/web_spotify/spotify/ServletInitializer.java
@@ -0,0 +1,18 @@
+package web_spotify.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServletInitializer.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ServletInitializer.class, args);
+    }
+}
