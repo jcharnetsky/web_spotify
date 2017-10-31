64cf88525785002ddaf205d0cb606c7b70ef7bf0
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-10-31 01:21:31 -0400

    Split main html and css files into multiple templated html and css files

---
+++ src/main/java/web_spotify/spotify/HomePageController.java
@@ -0,0 +1,16 @@
+package web_spotify.spotify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomePageController {
+
+    @RequestMapping("/index.html")
+    public String getMainPage(Model model, HttpSession session){
+        return "overview";
+    }
+}
