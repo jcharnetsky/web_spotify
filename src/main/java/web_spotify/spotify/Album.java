731c753b114eba0a7d310f1a76a8e4cfc036179e
James Morris <jamesamorris@yahoo.com>
2017-11-02 14:52:46 -0400

    Added the stubs to other classes

---
+++ src/main/java/web_spotify/spotify/Album.java
@@ -0,0 +1,9 @@
+package web_spotify.spotify;
+
+/**
+ *
+ * @author Cardinals
+ */
+class Album {
+    
+}
---
+++ src/main/java/web_spotify/spotify/Concert.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web_spotify.spotify;
+
+/**
+ *
+ * @author Cardinals
+ */
+class Concert {
+    
+}
