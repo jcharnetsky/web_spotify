f7344a187f8a12d590907f3288b0a695e45f2911
James Morris <jamesamorris@yahoo.com>
2017-11-04 16:19:27 -0400

    Added album

--- src/main/java/web_spotify/spotify/Album.java
+++ src/main/java/web_spotify/spotify/Album.java
@@ -1,9 +1,15 @@
 package web_spotify.spotify;
 
+import java.awt.Image;
+
 /**
- *
+ * A collection of songs that are an album
  * @author Cardinals
  */
-class Album {
+class Album extends SongCollection {
+    
+    public Album(int id, String title, User owner, Image image, String genre) {
+        super(id, title, owner, image, genre);
+    }
     
 }
