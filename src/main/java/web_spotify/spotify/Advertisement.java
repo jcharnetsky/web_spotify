b185828a25b720dbf557cd8c43c8db45ad26030e
user <user@localhost>
2017-11-04 18:50:40 -0400

    add equals method add javadoc for equals

--- src/main/java/web_spotify/spotify/Advertisement.java
+++ src/main/java/web_spotify/spotify/Advertisement.java
@@ -7,4 +7,16 @@ package web_spotify.spotify;
  */
 public class Advertisement {
 
+    /**
+     * Compare PremiumUser objects to determine equivalence
+     *
+     * @param object to compare
+     * @return True if object is an instance of Advertisement and has the same id; False otherwise
+     */
+    @Override
+    public boolean equals(Object a) {
+        if((a != null) && (a instanceof Advertisement)) {
+            return ((Advertisement) u).getId() == this.getId();
+        } else return false;
+    }
 }
