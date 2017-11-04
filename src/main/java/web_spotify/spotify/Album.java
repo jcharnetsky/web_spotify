f3ea369438498b8ca595c2e1eddf930afa5fdf35
James Morris <jamesamorris@yahoo.com>
2017-11-04 16:29:59 -0400

    Added full album class

--- src/main/java/web_spotify/spotify/Album.java
+++ src/main/java/web_spotify/spotify/Album.java
@@ -7,7 +7,15 @@ import java.awt.Image;
  * @author Cardinals
  */
 class Album extends SongCollection {
-    
+
+    /**
+     * The default constructor of the Album Class
+     * @param id The unique id of the collection
+     * @param title The title of the collection
+     * @param owner The owner of the collection
+     * @param image The image of the collection
+     * @param genre The genre of the collection
+     */
     public Album(int id, String title, User owner, Image image, String genre) {
         super(id, title, owner, image, genre);
     }
