0c38cbde3d70918548feed84eb0025f1509dc2f6
James Morris <jamesamorris@yahoo.com>
2017-11-04 18:32:39 -0400

    Began a jsonUTILS class

---
+++ src/main/java/Utils/JsonUtils.java
@@ -0,0 +1,13 @@
+package Utils;
+
+
+/**
+ * A utility for json returns
+ * @author Cardinals
+ */
+public class JsonUtils {
+    
+    static public String createError(String errMessage) {
+        
+    }
+}
