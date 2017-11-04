85e8391c60b026bfbc57bb27c73ec30006dd58af
user <user@localhost>
2017-11-04 18:51:37 -0400

    fix javadoc wording

--- src/main/java/web_spotify/spotify/Advertisement.java
+++ src/main/java/web_spotify/spotify/Advertisement.java
@@ -8,7 +8,7 @@ package web_spotify.spotify;
 public class Advertisement {
 
     /**
-     * Compare PremiumUser objects to determine equivalence
+     * Compare Advertisement objects to determine equivalence
      *
      * @param object to compare
      * @return True if object is an instance of Advertisement and has the same id; False otherwise
