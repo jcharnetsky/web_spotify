c922ff9c04010c2657b4d54da242d7d928461b6f
Michael Cheung <Michael@Michaels-MacBook-Pro.local>
2017-11-05 14:02:51 -0500

    Artist.java updated to be a subclass of User

--- src/main/java/web_spotify/spotify/Artist.java
+++ src/main/java/web_spotify/spotify/Artist.java
@@ -10,31 +10,31 @@ import java.util.ArrayList;
  * @author Cardinals
  */
 public class Artist extends User {
-
+	
+	 /**
+     * A description of the Artist
+     */
+    private String bio;
+    
     /**
-     * A collection of strings that represent previous artist aliases
+     * A collection of the artist's aliases
      */
     private Collection<String> aliases;
 
     /**
-     * A collection of songs that the Basic Artist owns
+     * A collection of songs that the artist owns
      */
     private Collection<Song> ownedSongs;
 
     /**
-     * A collection of albums that the Basic Artist owns
+     * A collection of albums that the artist owns
      */
     private Collection<Album> ownedAlbums;
 
     /**
-     * A collection of concerts that the Basic Artist has/is attended/attending
+     * A collection of concerts that the artist has/is attended/attending
      */
     private Collection<Concert> concerts;
-
-    /**
-     * A piece of text which is the bio for the artist
-     */
-    private String bio;
 
     /**
      * The constructor the BasicArtist
@@ -47,107 +47,19 @@ public class Artist extends User {
      * @param bio The bio of the BasicArtist
      * @param aliases A collection of aliases the artist goes by
      */
-    public Artist(int id, String name, String email, String address, Date birthday, String bio, String imageURL, Collection<String> aliases) {
-        // Call the constructor
+    public Artist(int id, String name, String email, String address, Date birthday, String imageURL, String bio, Collection<String> aliases) {
         super(id, name, email, address, birthday, imageURL);
-        // Set the bio
         this.bio = bio;
-        // Instantiate all the collections
+        this.aliases = new ArrayList<String>();
+        this.aliases.addAll(aliases);
         this.ownedSongs = new ArrayList<Song>();
         this.ownedAlbums = new ArrayList<Album>();
         this.concerts = new ArrayList<Concert>();
-        this.aliases = new ArrayList<String>();
-        // Add all aliases into the alias set
-        this.aliases.addAll(aliases);
-    }
-    
-    /**
-     * Set the bio of the artist
-     * @param bio
-     */
-    public void setBio(String bio) {
-    		this.bio = bio;
-    }
-    
-    /**
-     * Get the bio of the artist
-     */
-    public String getBio() {
-    		return bio;
-    }
-    
-    /**
-     * Add a song to an artist's owned songs
-     * @param song
-     */
-    public void addOwnedSong(Song song) {
-    		ownedSongs.add(song);
-    }
-    
-    /** 
-     * Remove a song from an artist's owned songs
-     * @param song
-     */
-    public void removeOwnedSong(Song song) {
-    		ownedSongs.remove(song);
-    }
-    
-    /**
-     * Get an artist's owned songs
-     */
-    public Collection<Song> getOwnedSongs() {
-    		return ownedSongs;
-    }
-    
-    /**
-     * Add an album to an artist's owned albums
-     * @param album
-     */
-    public void addOwnedAlbum(Album album) {
-    		ownedAlbums.add(album);
-    }
-    
-    /** 
-     * Remove an album from an artist's owned songs
-     * @param song
-     */
-    public void removeOwnedAlbum(Album album) {
-    		ownedAlbums.remove(album);
-    }
-    
-    /**
-     * Get an artist's owned albums
-     */
-    public Collection<Album> getOwnedAlbums() {
-    		return ownedAlbums;
-    }
-    
-    /**
-     * Add an concert to an artist's concerts
-     * @param concert
-     */
-    public void addConcerts(Concert concert) {
-    		concerts.add(concert);
-    }
-    
-    /** 
-     * Remove a concert from an artist's concerts
-     * @param concert
-     */
-    public void removeOwnedConcert(Concert concert) {
-    		concerts.remove(concert);
-    }
-    
-    /**
-     * Get an artist's concerts
-     */
-    public Collection<Concert> getConcerts() {
-    		return concerts;
     }
     
     /**
      * Add an alias to an artist's aliases
-     * @param string
+     * @param alias An alias
      */
     public void addAlias(String alias) {
     		aliases.add(alias);
@@ -155,29 +67,96 @@ public class Artist extends User {
     
     /** 
      * Remove an alias from an artist's aliases
-     * @param alias
+     * @param alias An alias
      */
     public void removeAlias(String alias) {
     		aliases.remove(alias);
     }
     
     /**
-     * Get an artist's aliases
+     * Add a song to an artist's owned songs
+     * @param song A song the artist owns
      */
+    public void addOwnedSong(Song song) {
+    		ownedSongs.add(song);
+    }
+    
+    /** 
+     * Remove a song from an artist's owned songs
+     * @param song A song the artist owns
+     */
+    public void removeOwnedSong(Song song) {
+    		ownedSongs.remove(song);
+    }
+    
+    /**
+     * Add an album to an artist's owned albums
+     * @param album An album the artist owns
+     */
+    public void addOwnedAlbum(Album album) {
+    		ownedAlbums.add(album);
+    }
+    
+    /** 
+     * Remove an album from an artist's owned songs
+     * @param album An album the artist owns
+     */
+    public void removeOwnedAlbum(Album album) {
+    		ownedAlbums.remove(album);
+    }
+    
+    /**
+     * Add an concert to an artist's concerts
+     * @param concert A concert the artist is performing at or hosting
+     */
+    public void addConcerts(Concert concert) {
+    		concerts.add(concert);
+    }
+    
+    /** 
+     * Remove a concert from an artist's concerts
+     * @param concert A concert the artist is performing at or hosting
+     */
+    public void removeOwnedConcert(Concert concert) {
+    		concerts.remove(concert);
+    }
+    
+	/**
+	* Compare BasicArtist objects to determine equivalence
+	*
+	* @param a object to compare
+	* @return True if object is an instance of BasicArtist and has the same id; False otherwise
+	*/
+	@Override
+	public boolean equals(Object a) {
+		if((a != null) && (a instanceof Artist)) {
+		    return ((Artist) a).getId() == this.getId();
+		} else return false;
+	}
+    
+    /* Getters */
+    public String getBio() {
+    		return bio;
+    }
+    
     public Collection<String> getAliases() {
     		return aliases;
     }
-                
-    /**
-     * Compare BasicArtist objects to determine equivalence
-     *
-     * @param a object to compare
-     * @return True if object is an instance of BasicArtist and has the same id; False otherwise
-     */
-    @Override
-    public boolean equals(Object a) {
-        if((a != null) && (a instanceof Artist)) {
-            return ((Artist) a).getId() == this.getId();
-        } else return false;
+    
+    public Collection<Song> getOwnedSongs() {
+    		return ownedSongs;
+    }
+    
+    public Collection<Album> getOwnedAlbums() {
+    		return ownedAlbums;
+    }
+    
+    public Collection<Concert> getConcerts() {
+    		return concerts;
+    }
+    
+    /* Setters */
+    public void setBio(String bio) {
+    		this.bio = bio;
     }
 }
