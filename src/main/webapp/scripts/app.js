91b4d0259440267e972c2328393c7659217d93d6
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-07 03:04:57 -0500

    Updated SpringExceptionto to use getMessage(), Added include for error.html to display and fade after 1s if an error message is receieved.

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -4,7 +4,7 @@ function loadToDiv(div, URL) {
     var node = document.getElementById(div);
 
     //Clear out whatever was in the content container
-    if(node == null || node.firstChild == null) return;
+    if(node == null) return;
     while(node.firstChild){
       node.removeChild(node.firstChild);
     }
@@ -27,14 +27,29 @@ app.controller('MainCtrl', function($compile, $scope, $http, $parse){
            // Send the XMLHttpRequest
            $http.get(location.origin + "/" + controllerPath).then(function(response) {
 
-             // Grab the JSON response and parse it straight to scope variables
-             $parse(controllerPath).assign($scope, response.data);
-             node = loadToDiv(div, URL);
-             $compile(node)($scope);
+              // If there was no error, proceed as usual
+             if(!response.data.error){
+
+                // Grab the JSON response and parse it straight to scope variables
+                $parse(controllerPath).assign($scope, response.data);
+                node = loadToDiv(div, URL);
+                $compile(node)($scope);
+
+             } else {
+
+                console.log(response.data);
+                // If there was an error, display an error pop-up
+                $parse('error').assign($scope, response.data.error);
+                node = loadToDiv('error_dialog', 'error.html');
+                $compile(node)($scope);
+             }
 
            }).catch(function (err) {
-             // Catch and print any server-side error to console
-             console.log(err)
+                 // Catch and print any server-side error to console
+                 console.log(err);
+                 $parse('error').assign($scope, err);
+                 node = loadToDiv('error_dialog', 'error.html');
+                 $compile(node)(scope);
            });
         } else {
            node = loadToDiv(div, URL);
