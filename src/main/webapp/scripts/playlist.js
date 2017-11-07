b7b63e474f626cb9061c863bf58e8a62f598f608
Josh Charnetsky <joshua.charnetsky@stonybrook.edu>
2017-11-07 18:05:12 -0500

    insert default values for playlist info remove text size of playlist song data increase add button size in playlist

--- src/main/webapp/scripts/playlist.js
+++ src/main/webapp/scripts/playlist.js
@@ -2,6 +2,9 @@ app = angular.module('web_spotify');
 
 app.controller('LoadPlaylistCtrl', function($compile, $scope, $http, $parse){
        console.log("LOAD PLAYLIST CALLED");
+    $scope.playlist_owner = "Owner";
+    $scope.playlist_song_num = "0";
+    $scope.playlist_duration = "0 min";
     $scope.loadPlaylist = function (div, URL, id) {
        controllerPath = "getPlaylistData";
 
