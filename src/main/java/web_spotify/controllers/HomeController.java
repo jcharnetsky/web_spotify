46b509ac91c230126b57df63b18ffe0011aea4b4
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-07 00:22:34 -0500

    Added SpotifyException, SpotifyExceptionHandler, and relevant documentation

--- src/main/java/web_spotify/controllers/HomeController.java
+++ src/main/java/web_spotify/controllers/HomeController.java
@@ -1,12 +1,8 @@
 package web_spotify.controllers;
 
-import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import web_spotify.spotify.Focus;
-import web_spotify.spotify.Chart;
 
 import javax.servlet.http.HttpSession;
 
---
+++ src/main/java/web_spotify/controllers/SpotifyExceptionHandler.java
@@ -0,0 +1,22 @@
+package web_spotify.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class SpotifyExceptionHandler {
+
+    /**
+     * Handle a thrown SpotifyException by return a JSON with error data for the client to display.
+     * @param e The exception to handle
+     * @return A JSON Object containing the error data in the exception
+     */
+    @ExceptionHandler(value = SpotifyException.class)
+    @RequestMapping(produces = "application/json")
+    @ResponseBody
+    public String spotifyErrorHandler(SpotifyException e){
+        return e.toJSONString();
+    }
+}
