4147cab56b50e2bb1eb80f5c8aa13c95ed93bc29
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-07 02:19:13 -0500

    Restructed app.js for async calls. Made playlists in sidebar load directly from getPlaylists in PlaylistController

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,43 +1,47 @@
 var app = angular.module('web_spotify',[]);
 
+function loadToDiv(div, URL) {
+    var node = document.getElementById(div);
+
+    //Clear out whatever was in the content container
+    if(node == null || node.firstChild == null) return;
+    while(node.firstChild){
+      node.removeChild(node.firstChild);
+    }
+
+    if(!(URL === 'null')){
+
+       //Add the new include to whatever was clicked
+       var tag = document.createElement('div');
+       tag.setAttribute("ng-include", "'templates/" + URL + "'");
+       node.appendChild(tag);
+    }
+    console.log(node);
+    return node;
+}
+
 app.controller('MainCtrl', function($compile, $scope, $http, $parse){
     $scope.loadTemplateAndJSON = function (div, URL, controllerPath) {
-        if(controllerPath != "null"){
+
+        if(!(controllerPath === 'null')){
 
            // Send the XMLHttpRequest
            $http.get(location.origin + "/" + controllerPath).then(function(response) {
 
              // Grab the JSON response and parse it straight to scope variables
              $parse(controllerPath).assign($scope, response.data);
+             node = loadToDiv(div, URL);
+             $compile(node)($scope);
 
            }).catch(function (err) {
              // Catch and print any server-side error to console
              console.log(err)
            });
+        } else {
+           node = loadToDiv(div, URL);
+           $compile(node)($scope);
         }
 
-        if(div == "null" || URL == "null"){
-           var node = document.getElementById(div);
-
-           //Clear out whatever was in the content container
-           if(node == null || node.firstChild == null) return;
-           while(node.firstChild){
-             node.removeChild(node.firstChild);
-           }
-
-           //Add the new include to whatever was clicked
-           var tag = document.createElement('div');
-           tag.setAttribute("ng-include", "'templates/" + URL + "'");
-           node.appendChild(tag);
-        }
-
-       //Compile the include
-       $compile(node)($scope);
-
-       //If you the navigateContent page was loaded, load the overview inside it
-       if(div == "main" && URL == "navigateContent.html"){
-
-       }
     }
 });
 
