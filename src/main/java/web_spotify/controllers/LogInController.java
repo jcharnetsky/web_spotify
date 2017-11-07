0aa2aa6d61492b54fb3332b9165dff5146ca4141
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-07 00:01:22 -0500

    Added documentation for the LogInController

--- src/main/java/web_spotify/controllers/LogInController.java
+++ src/main/java/web_spotify/controllers/LogInController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;s
 
 @Controller
 public class LogInController {
 
+    /**
+     * Direct the user to the login page. If there is a user logged in, direct them to
+     * the homepage.
+     * @param session Current session that may contain the logged in user.
+     * @return The login page html if the user is not logged in. The homepage html otherwise.
+     */
     @RequestMapping(value = "/attemptLogin", method = RequestMethod.GET)
     public String loginPage(HttpSession session) {
         if (session.getAttribute("User") == null) {
@@ -21,6 +26,17 @@ public class LogInController {
         return "homepage";
     }
 
+    /**
+     * Attempt to log in a user given their email and password credentials. If the email and password
+     * match a user account in the database, add the user to the session and return a JSON object containing a success
+     * message. Otherwise, return a JSON object containing an error saying that the credentials did not match. If a
+     * user is already logged in, also return an error saying that a user is logged in.
+     * @param email The given email of the user attempting to log in.
+     * @param password The given password of the user attempting to log in.
+     * @param session Current session that may contain a logged in user.
+     * @return If the credentials match and no user is logged in, a JSON containing a success message. Otherwise, a
+     * JSON containing a failure message.
+     */
     @RequestMapping(value = "/login", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String loginUser(@RequestParam(value = "email", required = true) String email,
@@ -51,6 +67,12 @@ public class LogInController {
         return valid ? JsonUtils.createBlankSuccess() : JsonUtils.createBlankError(errorMessage);
     }
 
+    /**
+     * Remove the user from the current session object and return a JSON object with a success message. If a user is
+     * not logged in, return a JSON object with a failure message.
+     * @param session Current session that may contain a logged in user
+     * @return If the user was successfully logged out, a success JSON object. Otherwise, a failure JSON object.
+     */
     @RequestMapping(value = "/logout", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String logoutUser(HttpSession session) {
