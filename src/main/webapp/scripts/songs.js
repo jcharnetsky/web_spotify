aef4e20410deb05847e69d5e0bfbe9c26d5c9f97
Josh Charnetsky <joshua.charnetsky@stonybrook.edu>
2017-11-07 18:19:11 -0500

    add initial songs list

---
+++ src/main/webapp/scripts/songs.js
@@ -0,0 +1,32 @@
+app = angular.module('web_spotify');
+
+app.controller('LoadPlaylistCtrl', function($compile, $scope, $http, $parse){
+    $scope.dynamic_button = "+";
+    $scope.loadPlaylist = function (div, URL, id) {
+       controllerPath = "getPlaylistData";
+
+       // Send the XMLHttpRequest
+       $http.get(location.origin + "/" + controllerPath, {params:{"playlistId": id}}).then(function(response) {
+
+          // If there was no error, proceed as usual
+         if(!response.data.error){
+
+            // Grab the JSON response and parse it straight to scope variables
+            $parse(controllerPath).assign($scope, response.data);
+            node = loadToDiv(div, URL);
+            $compile(node)($scope);
+
+         } else {
+
+            console.log(response.data);
+            // If there was an error, display an error pop-up
+            displayErrorPopup(response.data.error, $scope, $parse, $compile);
+         }
+
+       }).catch(function (err) {
+             // Catch and print any server-side error to console
+             displayErrorPopup(err, $scope, $parse, $compile);
+       });
+
+    }
+});
