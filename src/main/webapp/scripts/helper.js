7a3e53ac250a642b92bc8b4454718f4168c8926c
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-09 21:58:10 -0500

    Reformatted javascript to fit code style

--- src/main/webapp/scripts/helper.js
+++ src/main/webapp/scripts/helper.js
@@ -1,15 +1,10 @@
 function loadToDiv(div, URL) {
     var node = document.getElementById(div);
-
-    //Clear out whatever was in the content container
     if(node == null) return;
     while(node.firstChild){
       node.removeChild(node.firstChild);
     }
-
     if(!(URL === 'null')){
-
-       //Add the new include to whatever was clicked
        var tag = document.createElement('div');
        tag.setAttribute("ng-include", "'templates/" + URL + "'");
        node.appendChild(tag);
--- src/main/webapp/scripts/playlist.js
+++ src/main/webapp/scripts/playlist.js
@@ -1,34 +1,20 @@
-app = angular.module('web_spotify');
+angular.module('web_spotify').controller('LoadPlaylistCtrl', function($compile, $scope, $http, $parse){
+  $scope.playlist_owner = "Owner";
+  $scope.playlist_song_num = "0";
+  $scope.playlist_duration = "0 min";
+  $scope.loadPlaylist = function (div, URL, id) {
+    controllerPath = "getPlaylistData";
 
-app.controller('LoadPlaylistCtrl', function($compile, $scope, $http, $parse){
-    $scope.playlist_owner = "Owner";
-    $scope.playlist_song_num = "0";
-    $scope.playlist_duration = "0 min";
-    $scope.loadPlaylist = function (div, URL, id) {
-       controllerPath = "getPlaylistData";
-
-       // Send the XMLHttpRequest
-       $http.get(location.origin + "/" + controllerPath, {params:{"playlistId": id}}).then(function(response) {
-
-          // If there was no error, proceed as usual
-         if(!response.data.error){
-
-            // Grab the JSON response and parse it straight to scope variables
-            $parse(controllerPath).assign($scope, response.data);
-            node = loadToDiv(div, URL);
-            $compile(node)($scope);
-
-         } else {
-
-            console.log(response.data);
-            // If there was an error, display an error pop-up
-            displayErrorPopup(response.data.error, $scope, $parse, $compile);
-         }
-
-       }).catch(function (err) {
-             // Catch and print any server-side error to console
-             displayErrorPopup(err, $scope, $parse, $compile);
-       });
-
-    }
+    $http.get(location.origin + "/" + controllerPath, {params:{"playlistId": id}}).then(function(response) {
+      if(!response.data.error){
+         $parse(controllerPath).assign($scope, response.data);
+         node = loadToDiv(div, URL);
+         $compile(node)($scope);
+         return;
+      }
+      displayErrorPopup(response.data.error, $scope, $parse, $compile);
+    }).catch(function (err) {
+      displayErrorPopup(err, $scope, $parse, $compile);
+    });
+  }
 });
