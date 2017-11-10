18f5f46a3f1ff378c1329d6e36e458175b2fe892
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-09 21:07:25 -0500

    Reformatted java code to fit proper code style

--- src/main/java/web_spotify/controllers/HomeController.java
+++ src/main/java/web_spotify/controllers/HomeController.java
@@ -6,25 +6,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 
-
 @Controller
 public class HomeController {
 
-    /**
-     * Redirect the user to the home page. If the user is not logged in, send
-     * them to the user sign up page.
-     * @param session Current session that should contain the logged in user.
-     * @return The spotify homepage html if the user is logged in. The sign up page html otherwise.
-     */
-    @RequestMapping(value="/homepage", method= RequestMethod.GET)
-    public String getHomePage(HttpSession session){
-
-        // Check to make sure that the user is logged in to view this page
-        if(session.getAttribute("User") == null){
-            return "logIn.html";
-        }
-
-        // Grab content for page
-        return "home.html";
+  /**
+   * Redirect the user to the home page. If the user is not logged in, send
+   * them to the user sign up page.
+   * @param session Current session that should contain the logged in user.
+   * @return The spotify homepage html if the user is logged in. The sign up page html otherwise.
+   */
+  @RequestMapping(value = "/homepage", method = RequestMethod.GET)
+  public String getHomePage(HttpSession session) {
+    if (session.getAttribute("User") == null) {
+      return "logIn.html";
     }
+    return "home.html";
+  }
 }
--- src/main/java/web_spotify/controllers/ServletInitializer.java
+++ src/main/java/web_spotify/controllers/ServletInitializer.java
@@ -7,12 +7,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ServletInitializer.class);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(ServletInitializer.class);
+  }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(ServletInitializer.class, args);
-    }
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(ServletInitializer.class, args);
+  }
 }
