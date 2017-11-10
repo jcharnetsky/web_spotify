f542e70c67c6dc6ce626b3e43b566e3ae3eba0d5
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-09 21:58:10 -0500

    Reformatted javascript to fit code style

--- src/main/webapp/scripts/resizer.js
+++ src/main/webapp/scripts/resizer.js
@@ -1,40 +1,29 @@
-var app = angular.module('web_spotify');
-
-app.directive('resizer', function($document) {
-
+angular.module('web_spotify').directive('resizer', function($document) {
 	return function($scope, $element, $attrs) {
-
 		$element.on('mousedown', function(event) {
 			event.preventDefault();
-
 			$document.on('mousemove', mousemove);
 			$document.on('mouseup', mouseup);
 		});
 
 		function mousemove(event) {
-
-            // Handle vertical resizer
-            var x = event.pageX;
-            x = x * (100 / document.documentElement.clientWidth);
-
-            if ($attrs.resizerMax && x > $attrs.resizerMax) {
-                x = parseInt($attrs.resizerMax);
-            }
-            if ($attrs.resizerMin && x < $attrs.resizerMin) {
-                x = parseInt($attrs.resizerMin);
-            }
-
-            $element.css({
-                left: x + 'vw'
-            });
-
-            $($attrs.resizerLeft).css({
-                width: x + 'vw'
-            });
-            $($attrs.resizerRight).css({
-                left: (x + parseInt($attrs.resizerWidth)) + 'vw'
-            });
-
+      var x = event.pageX;
+      x = x * (100 / document.documentElement.clientWidth);
+      if ($attrs.resizerMax && x > $attrs.resizerMax) {
+          x = parseInt($attrs.resizerMax);
+      }
+      if ($attrs.resizerMin && x < $attrs.resizerMin) {
+          x = parseInt($attrs.resizerMin);
+      }
+      $element.css({
+          left: x + 'vw'
+      });
+      $($attrs.resizerLeft).css({
+          width: x + 'vw'
+      });
+      $($attrs.resizerRight).css({
+          left: (x + parseInt($attrs.resizerWidth)) + 'vw'
+      });
 		}
 
 		function mouseup() {
