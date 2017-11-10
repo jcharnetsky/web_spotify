f346c399d862a8b8129f7740407b1fc71bbd103c
James Morris <jamesamorris@yahoo.com>
2017-11-10 17:10:59 -0500

    Added new config settings

--- src/main/java/Utils/ConfigSettings.java
+++ src/main/java/Utils/ConfigSettings.java
@@ -7,6 +7,9 @@ package Utils;
 class ConfigSettings {
     public static final String DB_NAME = "cardinals";
     public static final String DB_HOST = "mysql2.cs.stonybrook.edu";
+    public static final String DB_USER = "cardinals";
     public static final String DB_PASS = "cardinals4life";
     public static final String DB_FORM = "jdbc:mysql://%s/%s?user=%s&password=%s";
+    
+    public static final String DB_LOGIN= "SELECT * FROM Users WHERE email='%s' AND password='%s';";
 }
