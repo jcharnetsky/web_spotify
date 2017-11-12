0ac624c59accd39ea3a868f4585d9ec64f712f00
James Morris <jamesamorris@yahoo.com>
2017-11-12 17:35:11 -0500

    Added changes to db

--- db/testData.sql
+++ db/testData.sql
@@ -1,14 +1,14 @@
 use cardinals;
 
-INSERT INTO Users VALUES
-    (0,'BASE', 'jack_the_guy@yahoo.com', 'pass', 'salt', 'Jack', 'Strong Island', TRUE, FALSE, NOW(), ''),
-    (1,'BASE', 'theJerryShore123@yahoo.com', 'pass', 'salt', 'Jerry', 'The Jersey Shore', TRUE, FALSE, NOW(), '')
+INSERT INTO Users(accStatus,email,password,salt,name,address,isPublic,isBanned,birthday,image) VALUES
+    ('BASE', 'jack_the_guy@yahoo.com', 'pass', 'salt', 'Jack', 'Strong Island', TRUE, FALSE, NOW(), ''),
+    ('BASE', 'theJerryShore123@yahoo.com', 'pass', 'salt', 'Jerry', 'The Jersey Shore', TRUE, FALSE, NOW(), '')
 ;
 
 INSERT INTO Playlists VALUES
-	(0, 'The Coolest Tracks ev3r', NOW(), TRUE, FALSE, 0, '', 'This is the coolest playlist ev3r');
+	(1, 'The Coolest Tracks ev3r', NOW(), TRUE, FALSE, 1, '', 'This is the coolest playlist ev3r');
 
 INSERT INTO PlaylistFollowers VALUES 
-	(0, 0),
-	(0, 1);
+	(1, 1),
+	(1, 2);
 
