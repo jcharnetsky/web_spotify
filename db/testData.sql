108d2656e6efe84c324e46365a65d45eca4621a0
James Morris <jamesamorris@yahoo.com>
2017-11-14 11:36:20 -0500

    more database changes

--- db/testData.sql
+++ db/testData.sql
@@ -1,9 +1,13 @@
 use cardinals;
 
-INSERT INTO Users(accStatus,email,password,salt,name,address,isPublic,isBanned,birthday,image) VALUES
-    ('BASE', 'jack_the_guy@yahoo.com', 'pass', 'salt', 'Jack', 'Strong Island', TRUE, FALSE, NOW(), ''),
-    ('BASE', 'theJerryShore123@yahoo.com', 'pass', 'salt', 'Jerry', 'The Jersey Shore', TRUE, FALSE, NOW(), '')
+INSERT INTO Users(userType,email,password,salt,name,address,isPublic,isBanned,isPremium,birthdate,image) VALUES
+    ('BASE','jack_the_guy@yahoo.com', 'pass', 'salt', 'Jack', 'Strong Island', TRUE, FALSE, TRUE, NOW(), ''),
+    ('BASE','theJerryShore123@yahoo.com', 'pass', 'salt', 'Jerry', 'The Jersey Shore', TRUE, FALSE, TRUE, NOW(), '')
 ;
+
+insert into reports(creatorId, subject, description, entity, entityID) values (1, 'Subject', 'description', 'USER', 1);
+insert into reports(creatorId, subject, description, entity, entityID) values (1, 'Subject', 'description', 'USER', 1);
+insert into reports(creatorId, subject, description, entity, entityID) values (1, 'Subject', 'description', 'USER', 1);
 
 INSERT INTO Playlists VALUES
 	(1, 'The Coolest Tracks ev3r', NOW(), TRUE, FALSE, 1, '', 'This is the coolest playlist ev3r');
