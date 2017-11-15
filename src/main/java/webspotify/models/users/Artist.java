f9430d5be9050ed252af5bcc6c89e45d11a1d42e
James Morris <jamesamorris@yahoo.com>
2017-11-15 13:29:30 -0500

    Added bidirectional on artist owned songs

--- src/main/java/webspotify/models/users/Artist.java
+++ src/main/java/webspotify/models/users/Artist.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import javax.persistence.*;
+import webspotify.models.media.Song;
 
 /**
  *
@@ -15,54 +16,64 @@ import javax.persistence.*;
 @DiscriminatorValue(value = "ARTIST")
 public class Artist extends User {
 
-	@Column(name = "stageName")
-	private String stageName;
-	@Column(name = "about")
-	private String about;
-	@ElementCollection
-	@CollectionTable(name = "artistAliases")
-	private List<String> aliases;
-	@ManyToMany(mappedBy = "artists")
-	private Set<Concert> concerts;
+    @Column(name = "stageName")
+    private String stageName;
+    @Column(name = "about")
+    private String about;
+    @ElementCollection
+    @CollectionTable(name = "artistAliases")
+    private List<String> aliases;
+    @ManyToMany(mappedBy = "artists")
+    private Set<Concert> concerts;
+    @OneToMany(mappedBy = "owner")
+    private List<Song> ownedSongs;
 
-	public Artist() {
-		super();
-		this.aliases = new ArrayList<String>();
-	}
+    public Artist() {
+        super();
+        this.aliases = new ArrayList<String>();
+    }
 
-	public String getStageName() {
-		return stageName;
-	}
+    public String getStageName() {
+        return stageName;
+    }
 
-	public void setStageName(String stageName) {
-		if (this.stageName != null) {
-			aliases.add(this.stageName);
-		}
-		this.stageName = stageName;
-	}
+    public void setStageName(String stageName) {
+        if (this.stageName != null) {
+            aliases.add(this.stageName);
+        }
+        this.stageName = stageName;
+    }
 
-	public String getAbout() {
-		return about;
-	}
+    public String getAbout() {
+        return about;
+    }
 
-	public void setAbout(String about) {
-		this.about = about;
-	}
+    public void setAbout(String about) {
+        this.about = about;
+    }
 
-	public List<String> getAliases() {
-		return aliases;
-	}
+    public List<String> getAliases() {
+        return aliases;
+    }
 
-	public void setAliases(List<String> aliases) {
-		this.aliases = aliases;
-	}
+    public void setAliases(List<String> aliases) {
+        this.aliases = aliases;
+    }
 
-	public Set<Concert> getConcerts() {
-		return concerts;
-	}
+    public Set<Concert> getConcerts() {
+        return concerts;
+    }
 
-	public void setConcerts(Set<Concert> concerts) {
-		this.concerts = concerts;
-	}
+    public void setConcerts(Set<Concert> concerts) {
+        this.concerts = concerts;
+    }
+
+    public List<Song> getOwnedSongs() {
+        return ownedSongs;
+    }
+
+    public void setOwnedSongs(List<Song> ownedSongs) {
+        this.ownedSongs = ownedSongs;
+    }
 
 }
