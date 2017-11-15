07d739e350cf99be09dc23b43ffa891cf1dd9cab
James Morris <jamesamorris@yahoo.com>
2017-11-14 20:05:44 -0500

    Added a SongResponse object which allows jackson to return a json reprsentation of only a certain number of fields

---
+++ src/main/java/webspotify/responses/SongResponse.java
@@ -0,0 +1,50 @@
+package webspotify.responses;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import webspotify.models.media.Song;
+import webspotify.models.users.Artist;
+import webspotify.models.users.Concert;
+
+/**
+ *
+ * @author Cardinals
+ */
+public class SongResponse {
+
+    private String trackName;
+    private Integer trackLength;
+    private ArtistResponse artist;
+
+    public SongResponse(Song build) {
+        this.trackLength = build.getTrackLength();
+        this.trackName = build.getTitle();
+        this.artist = new ArtistResponse(build.getOwner());
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public Integer getTrackLength() {
+        return trackLength;
+    }
+
+    public void setTrackLength(Integer trackLength) {
+        this.trackLength = trackLength;
+    }
+
+    public ArtistResponse getArtist() {
+        return artist;
+    }
+
+    public void setArtist(ArtistResponse artist) {
+        this.artist = artist;
+    }
+
+}
