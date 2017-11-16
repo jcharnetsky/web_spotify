8cd0e16e553631ced9fdb103f8fe643bde809dea
James Morris <jamesamorris@yahoo.com>
2017-11-16 15:03:13 -0500

    Add PlaylistCreateRequest for create playlist post

---
+++ src/main/java/webspotify/posts/PlaylistCreateRequest.java
@@ -0,0 +1,43 @@
+package webspotify.posts;
+
+import webspotify.types.GenreType;
+
+/**
+ *
+ * @author Cardinals
+ */
+public class PlaylistCreateRequest {
+
+    private String description;
+    private GenreType genre;
+    private String title;
+
+    public PlaylistCreateRequest() {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public GenreType getGenre() {
+        return genre;
+    }
+
+    public void setGenre(GenreType genre) {
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
