72bd2d92be7f63a4c23a76e1436edce92b1bab78
James Morris <jamesamorris@yahoo.com>
2017-11-16 15:03:42 -0500

    Added create playlist. Very rudimentary.

--- src/main/java/webspotify/controllers/rest/PlaylistController.java
+++ src/main/java/webspotify/controllers/rest/PlaylistController.java
@@ -6,9 +6,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import webspotify.Utilities.*;
-import webspotify.models.administration.Report;
 import webspotify.models.media.Playlist;
-import webspotify.models.users.User;
+import webspotify.posts.PlaylistCreateRequest;
 import webspotify.repo.PlaylistRepository;
 import webspotify.responses.PlaylistResponse;
 
@@ -20,33 +19,53 @@ import webspotify.responses.PlaylistResponse;
 @RequestMapping("/api/playlists")
 public class PlaylistController {
 
-    @Autowired
-    private PlaylistRepository playlistRepo;
+  @Autowired
+  private PlaylistRepository playlistRepo;
 
-    @GetMapping("/getData/{playlistId}")
-    public Response getPlaylistData(@PathVariable final int playlistId, HttpSession session) {
-        if (session.getAttribute("User") == null) {
-            return ResponseUtilities.filledFailure("User is not logged in.");
-        }
-        List<PlaylistResponse> response = new ArrayList<PlaylistResponse>();
-        Playlist playlist = playlistRepo.findOne(playlistId);
-        if(playlist == null) {
-            return ResponseUtilities.filledFailure("Playlist Does not exist");
-        }
-        response.add(new PlaylistResponse(playlist));
-        return ResponseUtilities.filledSuccess(response);
+  @GetMapping("/getData/{playlistId}")
+  public Response getPlaylistData(@PathVariable final int playlistId, HttpSession session) {
+    if (session.getAttribute("User") == null) {
+      return ResponseUtilities.filledFailure("User is not logged in.");
     }
+    List<PlaylistResponse> response = new ArrayList<PlaylistResponse>();
+    Playlist playlist = playlistRepo.findOne(playlistId);
+    if (playlist == null) {
+      return ResponseUtilities.filledFailure("Playlist Does not exist");
+    }
+    response.add(new PlaylistResponse(playlist));
+    return ResponseUtilities.filledSuccess(response);
+  }
 
-    @GetMapping("/getAll")
-    public Response getAllData(HttpSession session) {
-        if (session.getAttribute("User") == null) {
-            return ResponseUtilities.filledFailure("User is not logged in.");
-        }
-        List<PlaylistResponse> response = new ArrayList<PlaylistResponse>();
-        for (Playlist playlist : playlistRepo.findAll()) {
-            response.add(new PlaylistResponse(playlist));
-        }
-        return ResponseUtilities.filledSuccess(response);
+  @GetMapping("/getAll")
+  public Response getAllData(HttpSession session) {
+    if (session.getAttribute("User") == null) {
+      return ResponseUtilities.filledFailure("User is not logged in.");
     }
+    List<PlaylistResponse> response = new ArrayList<PlaylistResponse>();
+    for (Playlist playlist : playlistRepo.findAll()) {
+      response.add(new PlaylistResponse(playlist));
+    }
+    return ResponseUtilities.filledSuccess(response);
+  }
+
+  @PostMapping("/create")
+  public Response createPlaylist(@RequestBody PlaylistCreateRequest request, HttpSession session) {
+    if (session.getAttribute("User") == null) {
+      return ResponseUtilities.filledFailure("User is not logged in.");
+    }
+    Playlist playlistToAdd = new Playlist();
+    playlistToAdd.setBanned(false);
+    playlistToAdd.setPublic(true);
+    playlistToAdd.setCollaborative(false);
+    playlistToAdd.setDescription(request.getDescription());
+    playlistToAdd.setTitle(request.getTitle());
+    playlistToAdd.setGenre(request.getGenre());
+    Playlist added = playlistRepo.save(playlistToAdd);
+    if (added == null) {
+      return ResponseUtilities.filledFailure("Playlist Could not be Created");
+    } else {
+      return ResponseUtilities.emptySuccess();
+    }
+  }
 
 }
