1683f89805fb6080fc495d9d0a29b0e234ef582d
James Morris <jamesamorris@yahoo.com>
2017-11-17 15:37:31 -0500

    Added session utilities to abstract away from the session

---
+++ src/main/java/webspotify/Utilities/SessionUtilities.java
@@ -0,0 +1,18 @@
+package webspotify.Utilities;
+
+import javax.servlet.http.HttpSession;
+import webspotify.models.media.SongQueue;
+import webspotify.models.users.User;
+
+/**
+ *
+ * @author Cardinals
+ */
+public class SessionUtilities {
+  public static User getUserFromSession(HttpSession session) {
+    return (User)session.getAttribute("User");
+  }
+  public static SongQueue getSongQueueFromSession(HttpSession session) {
+    return (SongQueue)session.getAttribute("SongQueue");
+  }
+}
