44016b875b46fa61ba2227bfcd59e5332ee7c2bf
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-19 00:57:11 -0500

    Renamed LoginController to HomeController

--- src/main/java/webspotify/controllers/rest/HomeController.java
+++ src/main/java/webspotify/controllers/rest/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class LoginController {
+public class HomeController {
 
   /**
    * Direct the user to the login page. If there is a user logged in, direct them to
