3dad1b2dcbabd2cf2da380f815cd220ce67bb73a
James Morris <jamesamorris@yahoo.com>
2017-11-19 01:34:22 -0500

    Fix import spaghetti

--- src/main/java/webspotify/SvrInitialize.java
+++ src/main/java/webspotify/SvrInitialize.java
@@ -1,10 +1,7 @@
 package webspotify;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
