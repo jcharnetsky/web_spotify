3b6fc1ff4c32fab67d14fc640b1cb22654aaf5ee
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-19 02:23:49 -0500

    Restored sidebar playlist functionality

--- src/main/webapp/scripts/collection.js
+++ src/main/webapp/scripts/collection.js
@@ -1,6 +1,6 @@
 angular.module("web_spotify").controller("LoadCollectionCtrl", function($compile, $scope, $http, $parse){
   $scope.loadCollection = function (id) {
-    $http.get(location.origin + "/api/collections/" + id + "/get/info").then(function(response) {
+    $http.get(location.origin + "/api/playlists/" + id + "/get/info").then(function(response) {
       handleJSONResponse(response, "main", "playlist.html", "collection", $compile, $parse, $scope);
     }).catch(function (err) {
       displayErrorPopup(err, $scope, $parse, $compile);
