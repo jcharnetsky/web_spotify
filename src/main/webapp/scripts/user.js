09dbcb0b439d1f7090d2bccb1d8ceb72cf33dc05
Josh Charnetsky <joshua.charnetsky@protonmail.com>
2017-11-20 22:15:04 -0500

    Fix formatting for html and css files
    Removed whitespace
    Fixed indentation

--- src/main/webapp/scripts/user.js
+++ src/main/webapp/scripts/user.js
@@ -1,13 +1,13 @@
 angular.module("web_spotify").controller("UserCtrl", function ($compile, $scope, $http, $parse) {
-	$scope.loadUser = function (id) {
-		controllerPath = "api/users/get/" + id;
-		$http.get(location.origin + "/" + controllerPath).then(function (response) {
-			handleJSONResponse(response, "main", "user.html", "visitingUser", $compile, $parse, $scope);
-		}).catch(function (err) {
-			displayErrorPopup(err, $scope, $parse, $compile);
-		});
-	};
-	$scope.toggleDropdown = function (num) {
-		document.getElementById("other_dropdown_" + num).classList.toggle("show");
-	};
+		$scope.loadUser = function (id) {
+				controllerPath = "api/users/get/" + id;
+				$http.get(location.origin + "/" + controllerPath).then(function (response) {
+						handleJSONResponse(response, "main", "user.html", "visitingUser", $compile, $parse, $scope);
+				}).catch(function (err) {
+						displayErrorPopup(err, $scope, $parse, $compile);
+				});
+		};
+		$scope.toggleDropdown = function (num) {
+				document.getElementById("other_dropdown_" + num).classList.toggle("show");
+		};
 });
