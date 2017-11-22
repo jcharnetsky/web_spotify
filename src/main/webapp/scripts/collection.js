f55b578170142d3a21c78628280431f5fcd6e282
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-22 17:33:59 -0500

    Added modal to new palylist button

--- src/main/webapp/scripts/collection.js
+++ src/main/webapp/scripts/collection.js
@@ -1,9 +1,14 @@
-angular.module("web_spotify").controller("LoadCollectionCtrl", function($compile, $scope, $http, $parse){
+angular.module("web_spotify").controller("CollectionCtrl", function($compile, $scope, $http, $parse){
   $scope.loadCollection = function (id) {
     $http.get(location.origin + "/api/playlists/" + id + "/get/info").then(function(response) {
       handleJSONResponse(response, "main", "collection.html", "collection", $compile, $parse, $scope);
     }).catch(function (err) {
       displayErrorPopup(err, $scope, $parse, $compile);
     });
+  }
+
+  $scope.openPlaylistDialog = function() {
+    node = loadToDiv('modal_dialog', 'createPlaylist.html');
+    $compile(node)(scope);
   }
 });
