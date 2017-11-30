1a28526635f64a1366b2f430ff79eac25b2c47e9
Himanshu Kattelu <himanshukattelu@gmail.com>
2017-11-30 03:59:27 -0500

    Moved report code to manage.js, added manageReports

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -37,33 +37,6 @@ angular.module('web_spotify', ['mc.resizer']).controller('MainCtrl', function($c
       displayErrorPopup(err, $scope, $parse, $compile);
     });
   }
-
-  $scope.openReport = function(type, name, id) {
-    let response = {"data": {"content": {}}};
-    response.data.content.type = type;
-    response.data.content.entityName = name;
-    response.data.content.entityId = id;
-    handleJSONResponse(response, "modal_dialog", "report.html", "report", $compile, $parse, $scope);
-  }
-
-  $scope.createReport = function() {
-    data = JSON.stringify({
-      "subject": $scope.report_subject,
-      "description": $scope.report_description,
-      "entityType": $scope.report.type.toUpperCase(),
-      "entityId": $scope.report.entityId
-    })
-    $http.post("/api/reports/create", data, {headers: {"Content-Type": "application/json"}}).
-      then(function (response) {
-        if (!response.data.error) {
-          displayErrorPopup("Successfully made report", $scope, $parse, $compile);
-          return;
-        }
-        displayErrorPopup(response.data.errorMessage, $scope, $parse, $compile);
-      }).catch(function (err) {
-      displayErrorPopup(err, $scope, $parse, $compile);
-    });
-  }
 }).filter('secondsToMss', function($filter) {
     return function(seconds) {
       if(seconds < 600){
