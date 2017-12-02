c58fa430c1ebd934d6f5aac9752618367ecc7cec
James Morris <jamesamorris@yahoo.com>
2017-12-01 19:26:51 -0500

    Add album fix

--- src/main/java/webspotify/services/QueueService.java
+++ src/main/java/webspotify/services/QueueService.java
@@ -80,7 +80,7 @@ public class QueueService {
   public Response addAlbumToQueue(SongQueue queue, int albumId) {
     if (albumRepository.exists(albumId)) {
       Album albumToAdd = albumRepository.findOne(albumId);
-      queue.enqueueCollection(albumToAdd.getSongs());
+      queue.enqueueCollection(albumToAdd.getSongsInAlbum());
       return ResponseUtilities.emptySuccess();
     } else {
       return ResponseUtilities.filledFailure(ConfigConstants.COLLECTION_NO_EXIST);
